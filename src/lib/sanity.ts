7bfff3bdfb7e1d8172d9a7938ae9f3e7485f3c95
Alexander Santa Cruz Pacheco Ortega <alexandersantacruz32@gmail.com>
2024-07-20 14:52:06 -0400

    id arreglado

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -3,7 +3,7 @@ import imageUrlBuilder from '@sanity/image-url';
 
 function getSanityClient() {
 	return createClient({
-		projectId: '5sf89fxd',
+		projectId: 'v7tveygz',
 		dataset: 'production',
 		useCdn: true,
 		apiVersion: '2022-03-07',
@@ -15,7 +15,3 @@ export const client = getSanityClient();
 
 const builder = imageUrlBuilder(client);
 export const urlFor = (source) => builder.image(source);
-// token:
-// 	'skZJMWzcReCWwshntzIr9OGmCFEunZNzcRxLIhS7JQjP8r0oWPNuZc0wV61sglBgJAKNnPQrVq5sVZocYT68iTluqHbSsoFnEXFVU2bXJt1wc0kDLVnnGuI5vDOaljDHl8CXQHMelgUACRKf54SvlTLfaIihfw8nu8WQ3FmVer6Dha3c79Hj'
-// const client = getSanityClient();
-// client.delete({ query: '*[_type == "author" || _type == "post" || _type == "sanity.imageAsset"]' });
